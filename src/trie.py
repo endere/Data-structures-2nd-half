4bc1813c0e8baca0f3066f7c63248e1b97138b38
bonanashelby <bonanashelby@gmail.com>
2017-07-07 16:41:04 -0700

    trie tree working, refactoring to get the time down to more manageable

--- src/trie.py
+++ src/trie.py
@@ -4,10 +4,12 @@
 class Node(object):
     """Node object for trietree data structure."""
 
-    def __init__(self, value):
+    def __init__(self, value, level, parent):
         """Set up Node."""
+        self.parent = parent
         self.value = value
         self.next_list = []
+        self.level = level
 
 
 class TrieTree(object):
@@ -16,33 +18,42 @@ class TrieTree(object):
     def __init__(self):
         """Set up die tree."""
         self.length = 0
-        self.root = Node('.')
+        self.root = Node('.', 0, None)
 
     def insert(self, string):
         """Insert a node into the tree."""
         current = self.root
         for i in range(len(string)):
-            print('------------')
-            print('current letter:', string[i])
             if len(current.next_list) == 0:
-                print('it was an empty list.')
-                current.next_list.append(Node(string[i]))
+                current.next_list.append(Node(string[i], i + 1, current))
                 current = current.next_list[-1]
             else:
                 for node in current.next_list:
-                    print('current node value: ', node.value)
-                    if string[i] == node.value:
-                        print('node found in list.')
+                    if node == "$":
+                        continue
+                    elif string[i] == node.value:
                         current = node
                         break
                     elif node == current.next_list[-1]:
-                        print('node not found in list.')
-                        current.next_list.append(Node(string[i]))
+                        current.next_list.append(Node(string[i], i + 1, current))
                         current = current.next_list[-1]
+        current.next_list.append("$")
 
     def contains(self, string):
         """Return true if string in tree."""
-        pass
+        current = self.root
+        for i in range(len(string)):
+            for node in current.next_list:
+                if node == "$":
+                    continue
+                elif string[i] == node.value:
+                    current = node
+                elif node == current.next_list[-1]:
+                    return False
+        if "$" in current.next_list:
+            return True
+        else:
+            return False
 
     def size(self):
         """Return total number of words in tree."""
@@ -50,10 +61,43 @@ class TrieTree(object):
 
     def remove(self, string):
         """Remove string from tree."""
-        pass
+        current = self.root
+        for i in range(len(string)):
+            for node in current.next_list:
+                if node == "$":
+                    continue
+                elif string[i] == node.value:
+                    current = node
+                elif node == current.next_list[-1]:
+                    raise IndexError("String not in tree.")
+        current.next_list.remove("$")
+        while True:
+            child = current
+            current = current.parent
+            if len(current.next_list) > 1 or current == self.root:
+                current.next_list.remove(child)
+                break
 
 
 if __name__ == '__main__':
     test__the_trie_tree = TrieTree()
     print(test__the_trie_tree.insert('fire'))
-    print(test__the_trie_tree.insert('fire'))
+    print(test__the_trie_tree.insert('fig'))
+    print(test__the_trie_tree.contains('fire'))
+    print(test__the_trie_tree.contains('fig'))
+    print(test__the_trie_tree.contains('pig'))
+    print(test__the_trie_tree.remove('fire'))
+    print(test__the_trie_tree.contains('fire'))
+    print(test__the_trie_tree.contains('fig'))
+
+    with open('/usr/share/dict/words') as dictionary:
+        data = dictionary.read()
+        data = data.split('\n')
+    for i in range(len(data)):
+        test__the_trie_tree.insert(data[i])
+        print(len(data) - i)
+    print(test__the_trie_tree.contains('dinosaur'))
+    print(test__the_trie_tree.contains('skjdgjkdfkja'))
+    print(test__the_trie_tree.contains('fastidious'))
+    print(test__the_trie_tree.remove('pharaoh'))
+    print(test__the_trie_tree.contains('pharaoh'))
