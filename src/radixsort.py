9e47b8977524d31b9edb02c596fd88eb9618a9c1
bonanashelby <bonanashelby@gmail.com>
2017-07-16 16:44:24 -0700

    debugging

--- src/radixsort.py
+++ src/radixsort.py
@@ -1,8 +1,25 @@
-"""Merge sort data structure."""
+"""Radix sort data structure."""
 
 
 def radix_sort(array):
-    """Merge sort data structure function."""
+    """Radix sort data structure function."""
+    idx = 1  # the ones place
+    buckets = {'0': [], '1': [], '2': [], '3': [], '4': [], '5': [], '6': [], '7': [], '8': [], '9': []}
+    for i in range(len(str(max(array)))):
+        print("I is: ", i)
+        print(len(str(max(array))))
+        print("----------------")
+        for x in range(len(array)):
+            # print(int(str(array[x])[-i - 1]))
+            try:
+                buckets[str(array[x])[-i - 1]].append(array[x])
+            except IndexError:
+                buckets['0'].append(array[x])
+        to_return = []
+        for z in range(10):
+            to_return += buckets[str(z)]
+        print(to_return)
+        return to_return
 
 
 if __name__ == '__main__':
@@ -11,9 +28,10 @@ if __name__ == '__main__':
     from functools import reduce
     times = []
     num_runs = 5
-    string_length = 5
+    string_length = 1000
     for i in range(num_runs):
-        data = random.sample(range(string_length), string_length)
+        data = [1, 45, 20, 3, 345, 65, 2, 12, 888]
+        # data = random.sample(range(string_length), 10)
         timeA = datetime.datetime.now()
         print(radix_sort(data))
         timeB = datetime.datetime.now()
