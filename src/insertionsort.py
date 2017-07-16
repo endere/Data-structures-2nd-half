8459077413315e2ef346e437c2af08d3c0f3ba59
bonanashelby <bonanashelby@gmail.com>
2017-07-16 10:46:25 -0700

    insertion sort data structure.

--- src/insertionsort.py
+++ src/insertionsort.py
@@ -1,15 +1,19 @@
+"""Insertion Sort data structure."""
+
 
 def insertion_sort(array):
-    """."""
+    """Function for our insertion sort data structure."""
     for i in range(len(array)):
+        if not isinstance(array[i], int):
+            raise TypeError('Must be an integer, please try again.')
         for x in range(len(array[:i])):
             if array[i] < array[i - x - 1]:
                 if x == len(array[:i]) - 1:
                     array.insert(i - x - 1, array.pop(i))
             else:
-                array.insert(i - x, array.pop(i))  # (new index, remove old index)
+                array.insert(i - x, array.pop(i))
                 break
-    # array.insert(2, array.pop(4))
+
     return array
 
 
@@ -19,15 +23,12 @@ if __name__ == '__main__':
     from functools import reduce
     import sys
     times = []
-    num_runs = 5000
+    num_runs = 5
     string_length = 5
     for i in range(num_runs):
-        x = i / num_runs * 100
-        sys.stdout.write("\r%d%%" % x)
-        sys.stdout.flush()
         data = random.sample(range(string_length), string_length)
         timeA = datetime.datetime.now()
-        insertion_sort(data)
+        print(insertion_sort(data))
         timeB = datetime.datetime.now()
         times.append(timeB - timeA)
     average_time = reduce(lambda x, y: x + y, times) / len(times)
