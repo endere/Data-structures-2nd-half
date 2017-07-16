a73b45556fa47b736f4c9adf997245a1336b613a
bonanashelby <bonanashelby@gmail.com>
2017-07-16 10:16:02 -0700

    working on insert sort

--- src/insertionsort.py
+++ src/insertionsort.py
@@ -1,7 +1,19 @@
 
 def insertion_sort(array):
     """."""
-
+    for i in range(len(array)):
+        print("-------------------")
+        for x in range(len(array[:i])):
+            # print(array[:i])
+            if array[i] < array[i - x - 1]:
+                # print("We made it here")
+                pass
+            else:
+                # print("We made it here not")
+                print(array[i], array[i - x - 1])
+                array.insert(i - x, array.pop(i))  # (new index, remove old index)
+                break
+    return array
 
 
 if __name__ == '__main__':
@@ -10,14 +22,15 @@ if __name__ == '__main__':
     from functools import reduce
     import sys
     times = []
-    num_runs = 5000
+    num_runs = 1
     string_length = 5
     for i in range(num_runs):
-        x = i / num_runs * 100
-        sys.stdout.write("\r%d%%" % x)
-        sys.stdout.flush()
+        # x = i / num_runs * 100
+        # sys.stdout.write("\r%d%%" % x)
+        # sys.stdout.flush()
         data = random.sample(range(string_length), string_length)
         timeA = datetime.datetime.now()
+        print(data)
         print(insertion_sort(data))
         timeB = datetime.datetime.now()
         times.append(timeB - timeA)
