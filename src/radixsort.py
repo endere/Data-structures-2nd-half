f0fdc58a90010d94d80913572c7e28ec40044e45
Erik <erik.end@gmail.com>
2017-07-16 17:10:05 -0700

    sortdone

--- src/radixsort.py
+++ src/radixsort.py
@@ -3,23 +3,22 @@
 
 def radix_sort(array):
     """Radix sort data structure function."""
-    idx = 1  # the ones place
+    if len(array) <= 1:
+        return array
     buckets = {'0': [], '1': [], '2': [], '3': [], '4': [], '5': [], '6': [], '7': [], '8': [], '9': []}
     for i in range(len(str(max(array)))):
-        print("I is: ", i)
-        print(len(str(max(array))))
-        print("----------------")
         for x in range(len(array)):
-            # print(int(str(array[x])[-i - 1]))
+            if not isinstance(array[x], int):
+                raise TypeError('Must be an integer, please try again.')
             try:
                 buckets[str(array[x])[-i - 1]].append(array[x])
             except IndexError:
                 buckets['0'].append(array[x])
-        to_return = []
+        array = []
         for z in range(10):
-            to_return += buckets[str(z)]
-        print(to_return)
-        return to_return
+            array += buckets[str(z)]
+            buckets[str(z)] = []
+    return array
 
 
 if __name__ == '__main__':
@@ -30,8 +29,7 @@ if __name__ == '__main__':
     num_runs = 5
     string_length = 1000
     for i in range(num_runs):
-        data = [1, 45, 20, 3, 345, 65, 2, 12, 888]
-        # data = random.sample(range(string_length), 10)
+        data = random.sample(range(string_length), 10)
         timeA = datetime.datetime.now()
         print(radix_sort(data))
         timeB = datetime.datetime.now()
