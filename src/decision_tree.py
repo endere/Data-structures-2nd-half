740fef223f598a09c97ee8cf8e699249c4a9cd89
Erik <erik.end@gmail.com>
2017-07-20 15:49:36 -0700

    framework

---
+++ src/decision_tree.py
@@ -0,0 +1,22 @@
+"""."""
+
+
+Class DecisionTree(object):
+    
+
+    def __init__(self, max_depth=10, max_leaf_size=10):
+        pass
+
+
+    def fit(self, data):
+        """construct a decision tree based on some incoming data set; returns nothing"""
+        pass
+
+
+    def predict(self, data):
+        """returns labels for your test data."""
+        pass
+
+
+if __name__ == '__main__':
+    dimitri = DecisionTree()
