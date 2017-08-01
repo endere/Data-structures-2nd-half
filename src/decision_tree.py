95c5c94a54cdb6563327223088cc336c7de145ea
bonanashelby <bonanashelby@gmail.com>
2017-08-01 11:13:17 -0700

    working on getting back right data

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -31,18 +31,22 @@ class DecisionTree(object):
 
     def predict(self, data):
         """Return labels for your test data."""
+        df = pd.DataFrame(data, columns=["petal length (cm)", "petal width (cm)", "sepal length (cm)", "sepal width (cm)"])
         to_return = []
-        for i in data:
-            to_return.append(self.root(i))
+        for row in df.iterrows():
+            to_return.append(self.root(row))
         return to_return
 
-    def _cut_helper(self, data, axis, amount):
-        """Helper function for our fit function."""
-        # return the amount of both types on each side
-        left_side = []
-        right_side = []
-        for i in data:
-            print(i)
+    # def _cut_helper(self, data, axis, amount):
+    #     """Helper function for our fit function."""
+    #     # return the amount of both types on each side
+    #     left_side = []
+    #     right_side = []
+
+        # print(amount)
+        # print('--------------------')
+        # for i in data:
+        #     print(i)
         #     if i[axis] < amount:
         #         left_side.append(data["target"][i])
         #     else:
@@ -51,20 +55,68 @@ class DecisionTree(object):
 
     def fit(self, data):
         """A decision tree based on some incoming data set; returns nothing."""
+        best_score = float("inf")
         if type(data) == str or type(data) == tuple:
             smallest_length = min([i[0] for i in data])
             biggest_length = max([i[0] for i in data])
         else:
             smallest_length = data["petal length (cm)"].min()
             biggest_length = data["petal length (cm)"].max()
+        data = data.sort_values(["petal length (cm)"])
         for i in range(int(smallest_length * 10), int(biggest_length * 10)):
-            print(self._cut_helper(data, "petal length (cm)", i / 10))
-        print(smallest_length)
-        print(biggest_length)
-        # print([i[0] for i in data])
+            left_side = data[data["petal length (cm)"] < i / 10]
+            right_side = data[data["petal length (cm)"] >= i / 10]
+            score = left_side["target"].sum() + (len(right_side) - right_side["target"].sum())
+            if score < best_score:
+                best_left_side = left_side
+                best_right_side = right_side
+                best_score = score
+                best_axis = "petal length (cm)"
+                best_amount = i / 10
+        best_score = float("inf")
+        if type(data) == str or type(data) == tuple:
+            smallest_width = min([i[1] for i in data])
+            biggest_width = max([i[1] for i in data])
+        else:
+            smallest_width = data["petal width (cm)"].min()
+            biggest_width = data["petal width (cm)"].max()
+        data = data.sort_values(["petal width (cm)"])
+        for i in range(int(smallest_width * 10), int(biggest_width * 10)):
+            left_side = data[data["petal width (cm)"] < i / 10]
+            right_side = data[data["petal width (cm)"] >= i / 10]
+            score = left_side["target"].sum() + (len(right_side) - right_side["target"].sum())
+            if score < best_score:
+                best_left_side = left_side
+                best_right_side = right_side
+                best_score = score
+                best_axis = "petal width (cm)"
+                best_amount = i / 10
+        if not self.root:
+            if best_left_side["target"].sum() == 0 and len(best_right_side) - best_right_side["target"].sum() == 0:
+                self.root = QuestionNode(best_axis, best_amount, "setosa", "versicolor")
+            elif best_left_side["target"].sum() == 0:
+                self.root = QuestionNode(best_axis, best_amount, "setosa", self.fit(best_right_side))
+            elif len(best_right_side) - best_right_side["target"].sum() == 0:
+                self.root = QuestionNode(best_axis, best_amount, self.fit(best_left_side), "versicolor")
+            else:
+                self.root = QuestionNode(best_axis, best_amount, self.fit(best_left_side), self.fit(best_right_side))
+        else:
+            if best_left_side["target"].sum() == 0 and len(best_right_side) - best_right_side["target"].sum() == 0:
+                return QuestionNode(best_axis, best_amount, "setosa", "versicolor")
+            elif best_left_side["target"].sum() == 0:
+                return QuestionNode(best_axis, best_amount, "setosa", self.fit(best_right_side))
+            elif len(best_right_side) - best_right_side["target"].sum() == 0:
+                return QuestionNode(best_axis, best_amount, self.fit(best_left_side), "versicolor")
+            else:
+                return QuestionNode(best_axis, best_amount, self.fit(best_left_side), self.fit(best_right_side))
 
 
 if __name__ == '__main__':
     dimitri = DecisionTree()
     df = pd.read_csv("flowers_data.csv")
     dimitri.fit(df)
+    print(dimitri.predict([1.4, 0.1, 4.8, 3.0]))
+    # print(dimitri.root.axis)
+
+
+    
